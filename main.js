535d52a9cdcad50c3ecdcec73f428356519db955
Eunyoung Jang <bestsasa.sky@gmail.com>
2025-09-10 17:52:34 -0400

    second commit

--- main.js
+++ main.js
@@ -7,8 +7,12 @@ let taskList = [];
 
 function addTask() {
   //   console.log("clicked");
-  let taskContent = taskInput.value;
-  taskList.push(taskContent);
+  let task = {
+    id: randomIDGenerate(),
+    taskContent: taskInput.value,
+    isComplete: false,
+  };
+  taskList.push(task);
   console.log(taskList);
   render();
 }
@@ -16,13 +20,50 @@ function addTask() {
 function render() {
   let resultHTML = "";
   for (let i = 0; i < taskList.length; i++) {
-    resultHTML += `<div class="task">
-            <div>${taskList[i]}</div>
-            <div>
-              <button>check</button>
-              <button>Delete</button>
+    if (taskList[i].isComplete == true) {
+      resultHTML += `
+            <div class="task task-color">
+            <div class="task-done">${taskList[i].taskContent}</div>
+            <div class="icon">
+              <i onClick="toggleComplete('${taskList[i].id}')" class="fa-solid fa-thumbs-up fa-lg"></i>
+              <i onClick="deleteTask('${taskList[i].id}')" class="fa-regular fa-trash-can fa-lg"></i>
             </div>
           </div>`;
+    } else {
+      resultHTML += `<div class="task task-color">
+      <div>${taskList[i].taskContent}</div>
+      <div class="icon">
+        <i onClick="toggleComplete('${taskList[i].id}')" class="fa-regular fa-thumbs-up fa-lg"></i>
+        <i onClick="deleteTask('${taskList[i].id}')" class="fa-regular fa-trash-can fa-lg"></i>
+      </div>
+    </div>`;
+    }
   }
   document.getElementById("task-board").innerHTML = resultHTML;
+}
+
+function toggleComplete(id) {
+  console.log("id:", id);
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render();
+  //console.log(taskList);
+}
+
+function deleteTask(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id == id) {
+      taskList.splice(i, 1);
+      break;
+    }
+  }
+  render();
+}
+
+function randomIDGenerate() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
