d50649db4f96cf1e4d795d313f10e6a0fc45fd25
Eunyoung Jang <bestsasa.sky@gmail.com>
2025-09-11 19:51:19 -0400

    chap5 commit

--- main.js
+++ main.js
@@ -1,12 +1,35 @@
 let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add");
+let tabs = document.querySelectorAll(".task-tabs div");
+let underLine = document.getElementById("under-line");
+let underLineTabs = document.querySelectorAll(".task-tabs div:nth-child(n+2)");
+let mode = "all";
+let taskList = [];
+let filterList = [];
 
 addButton.addEventListener("click", addTask);
+underLineTabs.forEach((menu) =>
+  menu.addEventListener("click", (e) => tabsIndicator(e))
+);
 
-let taskList = [];
+taskInput.addEventListener("keypress", function (event) {
+  if (event.key === "Enter") {
+    addTask();
+  }
+});
+
+for (let i = 1; i < tabs.length; i++) {
+  tabs[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
 
 function addTask() {
   //   console.log("clicked");
+  if (taskInput.value.trim() === "") {
+    alert("할일을 입력해주세요");
+    return;
+  }
   let task = {
     id: randomIDGenerate(),
     taskContent: taskInput.value,
@@ -15,26 +38,35 @@ function addTask() {
   taskList.push(task);
   console.log(taskList);
   render();
+  taskInput.value = "";
+  taskInput.focus();
 }
 
 function render() {
+  let list = [];
+  if (mode === "all") {
+    list = taskList;
+  } else if (mode === "ongoing" || mode === "done") {
+    list = filterList;
+  }
+
   let resultHTML = "";
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete == true) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete == true) {
       resultHTML += `
             <div class="task">
-            <div class="task-done">${taskList[i].taskContent}</div>
+            <div class="task-done">${list[i].taskContent}</div>
             <div class="icon">
-              <i onClick="toggleComplete('${taskList[i].id}')" class="fa-solid fa-thumbs-up fa-lg"></i>
-              <i onClick="deleteTask('${taskList[i].id}')" class="fa-regular fa-trash-can fa-lg"></i>
+              <i onClick="toggleComplete('${list[i].id}')" class="fa-solid fa-thumbs-up fa-lg"></i>
+              <i onClick="deleteTask('${list[i].id}')" class="fa-regular fa-trash-can fa-lg"></i>
             </div>
           </div>`;
     } else {
       resultHTML += `<div class="task">
-     <div class="task-content">${taskList[i].taskContent}</div>
+     <div class="task-content">${list[i].taskContent}</div>
       <div class="icon">
-        <i onClick="toggleComplete('${taskList[i].id}')" class="fa-regular fa-thumbs-up fa-lg"></i>
-        <i onClick="deleteTask('${taskList[i].id}')" class="fa-regular fa-trash-can fa-lg"></i>
+        <i onClick="toggleComplete('${list[i].id}')" class="fa-regular fa-thumbs-up fa-lg"></i>
+        <i onClick="deleteTask('${list[i].id}')" class="fa-regular fa-trash-can fa-lg"></i>
       </div>
     </div>`;
     }
@@ -50,6 +82,7 @@ function toggleComplete(id) {
       break;
     }
   }
+  mode = "all";
   render();
   //console.log(taskList);
 }
@@ -61,9 +94,44 @@ function deleteTask(id) {
       break;
     }
   }
+  mode = "all";
   render();
 }
 
+function filter(event) {
+  console.log("filter", event.target);
+  filterList = [];
+  mode = event.target.id;
+  tabsIndicator(event);
+  if (mode === "all") {
+    //전체 리스트를 보여준다.
+    render();
+  } else if (mode === "ongoing") {
+    //진행중인 아이템을 보여준다.
+    //task.isComplete=false
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === false) {
+        filterList.push(taskList[i]);
+      }
+    }
+    render();
+  } else if (mode === "done") {
+    //끝난 아이템을 보여준다.
+    //task.isComplete=true
+    for (let i = 0; i < taskList.length; i++) {
+      if (taskList[i].isComplete === true) {
+        filterList.push(taskList[i]);
+      }
+    }
+  }
+  render();
+}
+
+function tabsIndicator(e) {
+  underLine.style.left = e.currentTarget.offsetLeft + "px";
+  underLine.style.width = e.currentTarget.offsetWidth + "px";
+  e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
+}
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
