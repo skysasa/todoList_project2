67f90865afec8a40281f16f5c68cbe00846e5883
Eunyoung Jang <bestsasa.sky@gmail.com>
2025-09-12 19:33:39 -0400

    first final commit

--- main.js
+++ main.js
@@ -2,15 +2,11 @@ let taskInput = document.getElementById("task-input");
 let addButton = document.getElementById("add");
 let tabs = document.querySelectorAll(".task-tabs div");
 let underLine = document.getElementById("under-line");
-let underLineTabs = document.querySelectorAll(".task-tabs div:nth-child(n+2)");
 let mode = "all";
 let taskList = [];
 let filterList = [];
 
 addButton.addEventListener("click", addTask);
-underLineTabs.forEach((menu) =>
-  menu.addEventListener("click", (e) => tabsIndicator(e))
-);
 
 taskInput.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -46,7 +42,8 @@ function render() {
   let list = [];
   if (mode === "all") {
     list = taskList;
-  } else if (mode === "ongoing" || mode === "done") {
+  } else {
+    //if (mode === "ongoing" || mode === "done") {
     list = filterList;
   }
 
@@ -82,8 +79,7 @@ function toggleComplete(id) {
       break;
     }
   }
-  mode = "all";
-  render();
+  filter();
   //console.log(taskList);
 }
 
@@ -94,19 +90,20 @@ function deleteTask(id) {
       break;
     }
   }
-  mode = "all";
-  render();
+  filter();
 }
 
 function filter(event) {
-  console.log("filter", event.target);
+  if (event) {
+    mode = event.target.id;
+    underLine.style.width = event.target.offsetWidth + "px";
+    underLine.style.left = event.target.offsetLeft + "px";
+    underLine.style.top =
+      event.target.offsetTop + (event.target.offsetHeight - 4) + "px";
+  }
+
   filterList = [];
-  mode = event.target.id;
-  tabsIndicator(event);
-  if (mode === "all") {
-    //전체 리스트를 보여준다.
-    render();
-  } else if (mode === "ongoing") {
+  if (mode === "ongoing") {
     //진행중인 아이템을 보여준다.
     //task.isComplete=false
     for (let i = 0; i < taskList.length; i++) {
@@ -114,8 +111,8 @@ function filter(event) {
         filterList.push(taskList[i]);
       }
     }
-    render();
   } else if (mode === "done") {
+    filterList = [];
     //끝난 아이템을 보여준다.
     //task.isComplete=true
     for (let i = 0; i < taskList.length; i++) {
@@ -127,11 +124,6 @@ function filter(event) {
   render();
 }
 
-function tabsIndicator(e) {
-  underLine.style.left = e.currentTarget.offsetLeft + "px";
-  underLine.style.width = e.currentTarget.offsetWidth + "px";
-  e.currentTarget.offsetTop + e.currentTarget.offsetHeight + "px";
-}
 function randomIDGenerate() {
   return "_" + Math.random().toString(36).substr(2, 9);
 }
